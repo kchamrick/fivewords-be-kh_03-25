8ac055986f0929971a8386567ca21c2cf4bbe59a
Kathleen Hamrick <kchamrick@gmail.com>
2025-03-28 13:32:31 -0400

    BE-KH-032825

---
+++ app.js
@@ -0,0 +1,44 @@
+require("dotenv").config({ path: "/.env" });
+const { client } = require("./server/db/index.js");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Use the API routes defined in the index.js file
+app.use("/api", require("./server/api"));
+
+if (
+    !process.env.PGUSER ||
+    !process.env.PGHOST ||
+    !process.env.PGDATABASE ||
+    !process.env.PGPASSWORD ||
+    !process.env.PGPORT
+) {
+    console.error("One or more environment variables are missing.");
+    process.exit(1);
+}
+
+const init = async () => {
+    try {
+        await client.connect();
+        console.log("connected to database");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("connection error:", error);
+    }
+};
+
+init();
