a6afc2e90e91689513651882e2f7b202d948d48f
Kathleen Hamrick <kchamrick@gmail.com>
2025-03-28 14:02:04 -0400

    BE-2-KH-032825

--- server/db.js
+++ server/db.js
@@ -1,5 +1,28 @@
-const pg = require("pg");
+const { Pool } = require("pg");
+require ('dotenv').config();
 
-const client = new pg.Client();
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
 
-module.exports = {client};
+//
+const testConnection = async() => {
+    try{
+        const client = await pool.connect();
+        console.log('connected to PostgreSQL database');
+        client.release();
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+};
+
+testConnection();
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+    pool
+};
