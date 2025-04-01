f551f24aa42d86b76ab09ae115847433c3be0b6a
Kathleen Hamrick <kchamrick@gmail.com>
2025-03-31 21:04:25 -0400

    MIDDLEWARE-1-kh-033125

--- server/index.js
+++ server/index.js
@@ -5,14 +5,17 @@ require("dotenv").config();
 
 const db = require("./db/db.js");
 const { createTables } = require("./db/schema.js");
+const apiRouter = require ('./api');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req,res,next)=>{
-    res.send("hello from server");
-})
+//Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to Five Words Poetry Game API')
+});
 
 const init = async ()=>{
     try {
